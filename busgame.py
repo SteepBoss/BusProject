9af055e1744b48013d93330a41100c8cb24d6db6
SteepBoss <aspirenumeric@gmail.com>
2023-12-15 21:59:14 +0200

    add bus

--- busgame.py
+++ busgame.py
@@ -1,26 +1,54 @@
-import pygame
+import pygame as pg
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
+pg.init()
+
+# Размеры экрана
+screen_width, screen_height = 1280, 720
+screen = pg.display.set_mode((screen_width, screen_height))
+
+clock = pg.time.Clock()
+
+bus = pg.Rect(600, 100, 300, 500)
+bus_outline_color = (255, 255, 255)
+
+# Скорость прямоугольника
+speed = 1
+
+# Создаем список прямоугольников
+rectangles = [{'x': 0, 'y': 360, 'direction': 1}]
+
 running = True
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_q:
+                running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
+    # Проверяем, достиг ли текущий прямоугольник правого края
+    if rectangles[-1]['x'] >= screen_width - 30:
+        # Создаем новый прямоугольник
+        new_rect = {'x': 0, 'y': 360, 'direction': 1}
+        rectangles.append(new_rect)
 
-    # RENDER YOUR GAME HERE
+    # Обновляем координаты каждого прямоугольника
+    for rect in rectangles:
+        rect['x'] += speed * rect['direction']
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+        # Изменяем направление, если прямоугольник достиг границы экрана
+        if rect['x'] <= 0 or rect['x'] >= screen_width - 30:
+            rect['direction'] *= -1
+    screen.fill((0, 0, 0))
 
-    clock.tick(60)  # limits FPS to 60
+    pg.draw.rect(screen, bus_outline_color, bus ,2)
 
-pygame.quit()
+    # Отрисовка всех прямоугольников
+    for rect in rectangles:
+        pg.draw.rect(screen, (0, 128, 0), (rect['x'], rect['y'], 30, 30))
+    pg.display.flip()
+
+    clock.tick(140)
+
+pg.quit()
