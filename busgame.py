76a70b2c0eb7cb4629e7fb273fb094379ece2235
SteepBoss <aspirenumeric@gmail.com>
2023-12-18 21:42:48 +0200

    fix green square

--- busgame.py
+++ busgame.py
@@ -11,14 +11,12 @@ screen = pg.display.set_mode((screen_width, screen_height))
 
 clock = pg.time.Clock()
 
-bus = pg.Rect(600, 100, 300, 300)
+bus = pg.Rect(600, 100, 600, 300)
 bus_outline_color =  (255, 0, 0)
 
 # Скорость прямоугольника
 speed = 3
 
-# Высота левой стороны автобуса
-left_side_bus = 350
 
 # Создаем список прямоугольников
 rectangles = [{'x': 0, 'y': 360, 'direction': 1}]
@@ -41,16 +39,17 @@ while running:
             # Меняем направление на противоположное только если зеленый прямоугольник справа от автобуса
             if rect['direction'] == 1 and rect['x'] + 30 > bus.right:
                 rect['direction'] *= -1
-
-    screen.fill((0, 0, 0))
+        if rect['x'] < 0:
+            rect['direction'] *= -1
+        pg.draw.rect(screen, (0, 128, 0), (rect['x'], rect['y'], 30, 30))
+    # Высота левой стороны автобуса
+    left_side_bus = 350
     pg.draw.line(screen, bus_outline_color, bus.topleft, (bus.left, left_side_bus), 2)
     pg.draw.line(screen, bus_outline_color, bus.topleft, bus.topright, 2)
     pg.draw.line(screen, bus_outline_color, bus.bottomleft, bus.bottomright, 2)
     pg.draw.line(screen, bus_outline_color, bus.topright, bus.bottomright, 2)
-
-    for rect in rectangles:
-        pg.draw.rect(screen, (0, 128, 0), (rect['x'], rect['y'], 30, 30))
     pg.display.flip()
+    screen.fill((0, 0, 0))
 
     clock.tick(140)
 
