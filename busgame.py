5f3417b333aaeff436c323224f25a666a7505b31
SteepBoss <aspirenumeric@gmail.com>
2023-12-19 14:26:29 +0200

    Добавил места для сидения

--- busgame.py
+++ busgame.py
@@ -6,20 +6,25 @@ pg.init()
 pg.display.set_caption("SimulationVirus")
 
 # Размеры экрана
-screen_width, screen_height = 1280, 720
+screen_width, screen_height = 1720, 920
 screen = pg.display.set_mode((screen_width, screen_height))
 
 clock = pg.time.Clock()
-
-bus = pg.Rect(600, 100, 600, 300)
-bus_outline_color =  (255, 0, 0)
+# X-координата верхнего левого угла: 600 Y-координата верхнего левого угла: 100
+# ШИРИНА  , ВЫСОТА
+bus = pg.Rect(1000, 100, 300, 740)
+bus_outline_color = (255, 0, 0)
 
 # Скорость прямоугольника
 speed = 3
 
-
 # Создаем список прямоугольников
-rectangles = [{'x': 0, 'y': 360, 'direction': 1}]
+humans = [{'x': 0, 'y': 800, 'direction': 1}]
+
+rect_width = 30
+rect_height = 30
+num_rectangles_horizontal = 2
+num_rectangles_vertical = 11
 
 running = True
 
@@ -32,18 +37,30 @@ while running:
                 running = False
 
     # Обновляем координаты каждого прямоугольника
-    for rect in rectangles:
-        rect['x'] += speed * rect['direction']
+    for human in humans:
+        human['x'] += speed * human['direction']
         # Проверяем столкновение с автобусом
-        if bus.colliderect(pg.Rect(rect['x'], rect['y'], 30, 30)):
+        if bus.colliderect(pg.Rect(human['x'], human['y'], 30, 30)):
             # Меняем направление на противоположное только если зеленый прямоугольник справа от автобуса
-            if rect['direction'] == 1 and rect['x'] + 30 > bus.right:
-                rect['direction'] *= -1
-        if rect['x'] < 0:
-            rect['direction'] *= -1
-        pg.draw.rect(screen, (0, 128, 0), (rect['x'], rect['y'], 30, 30))
+            if human['direction'] == 1 and human['x'] + 30 > bus.right:
+                human['direction'] *= -1
+        if human['x'] < 0 or human['x'] + 30 > screen_width:
+            human['direction'] *= -1
+
+        pg.draw.rect(screen, (0, 128, 0), (human['x'], human['y'], 30, 30))
+    for i in range(num_rectangles_horizontal):
+        for j in range(num_rectangles_vertical):
+            x = 30 + bus.x + 60 * i  # Размещение прямоугольников по горизонтали
+            y = 30 + bus.y + 60 * j  # Размещение прямоугольников по вертикали
+            pg.draw.rect(screen, (255, 0, 0), (x, y, rect_width, rect_height))
+    for i in range(num_rectangles_horizontal):
+        for j in range(num_rectangles_vertical):
+            x = 180 + bus.x + 60 * i  # Размещение прямоугольников по горизонтали
+            y = 30 + bus.y + 60 * j  # Размещение прямоугольников по вертикали
+            pg.draw.rect(screen, (255, 0, 0), (x, y, rect_width, rect_height))
     # Высота левой стороны автобуса
-    left_side_bus = 350
+    left_side_bus = 780
+
     pg.draw.line(screen, bus_outline_color, bus.topleft, (bus.left, left_side_bus), 2)
     pg.draw.line(screen, bus_outline_color, bus.topleft, bus.topright, 2)
     pg.draw.line(screen, bus_outline_color, bus.bottomleft, bus.bottomright, 2)
